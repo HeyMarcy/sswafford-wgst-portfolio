ee476c7dfce44c1fe4087023112f0bbc4fb4be21
Nick Breaton <nick@breaton.com>
2016-11-14 13:36:53 -0500

    Setup html converter with mammoth

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+import axios from 'axios';
+import mammoth from 'mammoth';
+
+axios.get('/journals/Journal 2.docx', {
+  responseType: 'arraybuffer'
+})
+  .then(res => res.data)
+  .then(arrayBuffer => mammoth.convertToHtml({ arrayBuffer }))
+  .then(result => result.value)
+  .then(data => {
+    document.getElementById('root').innerHTML = data;
+  })
